8b1521ea6f55b52e2a17c3e8799c0d1924bd610e
Luigi Maselli <luigi+git@grigio.org>
2019-02-04 19:21:14 +0100

    updated broken scatter and endpont

--- src/eosfilestore/costants.ts
+++ src/eosfilestore/costants.ts
@@ -51,7 +51,7 @@ function networkConfigFn() {
   const nc = {
     blockchain: 'eos',
     protocol: 'https',
-    host: 'api1.eosasia.one',
+    host: 'eos.greymass.com',
     port: 443,
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
   }
